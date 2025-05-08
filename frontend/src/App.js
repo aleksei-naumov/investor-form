c3fb0670d6e1ef3a5472f8f4c5fb143abd708b72
Alex Naumov <naumov78@gmail.com>
2025-05-08 00:43:56 -0400

    Add Tailwind setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,13 @@
+import "./index.css";
+
 function App() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <div className="text-green-600 text-2xl border-2 border-red-500">
+        Testing Tailwind
+      </div>
+    </div>
+  );
 }
 
 export default App;
